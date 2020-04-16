2ad35e04dc857c01d8de7436f0cd3d496b669699
Steve_Zhang <wentaozhang0305@gmail.com>
2020-04-16 15:02:18 -0700

    Updates

---
+++ precache-manifest.e3064f9fc3e7aee07feb121e539ed6ac.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41b3f54e6cdacd7192d6fe25f26a9e2",
+    "url": "/Abstrakt-Website/index.html"
+  },
+  {
+    "revision": "1e775a3d289b973f8575",
+    "url": "/Abstrakt-Website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dc635d6cd023684bbb4c",
+    "url": "/Abstrakt-Website/static/js/2.b1cd5c2e.chunk.js"
+  },
+  {
+    "revision": "40f6ddb379fd6d5397eded99d3b7f8f9",
+    "url": "/Abstrakt-Website/static/js/2.b1cd5c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e775a3d289b973f8575",
+    "url": "/Abstrakt-Website/static/js/main.45c524b5.chunk.js"
+  },
+  {
+    "revision": "4a0f33598b667129fb76",
+    "url": "/Abstrakt-Website/static/js/runtime-main.007edbfb.js"
+  },
+  {
+    "revision": "566a63cbe7e172715251a2bcc1e65f31",
+    "url": "/Abstrakt-Website/static/media/ABSTRAKT_black.566a63cb.png"
+  },
+  {
+    "revision": "9f64540bfad5f7dc195a1336d6f050a7",
+    "url": "/Abstrakt-Website/static/media/banner.9f64540b.jpg"
+  },
+  {
+    "revision": "35ca1db70dd25e6d01c706997d4f5b2a",
+    "url": "/Abstrakt-Website/static/media/team_photo.35ca1db7.JPG"
+  }
+]);
